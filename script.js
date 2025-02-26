cfdba55137bb0f790d9532e538f10c3c16c661d3
R1dzzz <gorid772@gmail.com>
2025-02-27 01:26:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ async function fetchGeminiResponse(userMessage) {
 window.addEventListener("load", () => {
     addMessageToChat(
         "ai",
-        "Say Hi for Minerva AI from Pandansari 🤙🗿"
+        "Say Hi to Minerva AI from Pandansari 🤙🗿"
     );
 
     // Sembunyikan animasi pembuka setelah 3 detik
